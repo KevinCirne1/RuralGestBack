a33b668dee9bb9cf4e0f83ef956b0f696375ce58
MatheusSantos007 <matheuserafim41@gmail.com>
2025-09-04 16:17:58 -0300

    Atualização para main

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
-    # --- Configurações do JWT ---
+    # --- Configurações do JWT --
     #JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     #JWT_TOKEN_LOCATION = ['headers']
     #JWT_HEADER_NAME = 'Authorization'
