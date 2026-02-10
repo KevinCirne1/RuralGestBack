e825449f1ad88f3efd800de4c2958c387220d5bc
MatheusSantos007 <matheuserafim41@gmail.com>
2026-02-10 18:57:25 -0300

    excluir

--- criar_tabelas.py
+++
@@ -1,9 +0,0 @@
-# criar_tabelas.py
-from app import app  # Importe sua aplicação principal
-from helpers.database import db # Onde fica sua configuração de banco
-from models.notificacao import Notificacao # Importante importar para o SQLAlchemy "ver" a tabela nova
-
-# Força a aplicação a entrar no contexto para acessar o banco
-with app.app_context():
-    db.create_all()
-    print("✅ Sucesso! Tabela 'notificacao' criada (se ela não existia).")
